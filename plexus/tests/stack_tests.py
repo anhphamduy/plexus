617b33b738e1ae556bfc4af483b906fb92e2aa79
Anh Pham <anhphamduy@outlook.com>
2021-04-16 00:32:59 +1000

    Add basic structure for tests

---
+++ plexus/tests/stack_tests.py
@@ -0,0 +1,24 @@
+import unittest
+
+from plexus import Stack
+
+
+class StackTests(unittest.TestCase):
+
+    def test_pour_for_one_level_stack(self):
+        raise NotImplementedError()
+
+    def test_pour_for_two_level_stack(self):
+        raise NotImplementedError()
+
+    def test_pour_for_three_level_stack(self):
+        raise NotImplementedError()
+
+    def test_get_glass_from_one_level_stack(self):
+        raise NotImplementedError()
+
+    def test_get_glass_from_two_level_stack(self):
+        raise NotImplementedError()
+
+    def test_get_glass_from_three_level_stack(self):
+        raise NotImplementedError()
