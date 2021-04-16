f77b632fd7d16e6369aa18f727e52f5e082ae439
Anh Pham <anhphamduy@outlook.com>
2021-04-16 10:30:56 +1000

    Implement human pour method

--- plexus/stack.py
+++ plexus/stack.py
@@ -22,18 +22,6 @@ class Stack:
         The number of levels that the stack has.
     glasses: Glass[]
         A list contains the glasses of the stack.
-
-
-    Examples
-    --------
-    >>> from plexus import Stack
-    >>> stack = Stack(n_levels=2)
-    >>> stack.pour(2000)
-    [Glass(current_capacity=250), Glass(current_capacity=250), Glass(current_capacity=250)]
-    >>> print(stack.get_glass(0, 0))
-    250
-    >>> print(stack.get_glass(1, 0))
-    250
     """
 
     def __init__(self, n_levels):
@@ -70,12 +58,42 @@ class Human:
         Pours water into the stack of glasses
         :param stack: Stack
             The stack to be poured into
-        :param water_amount: number
+        :param water_amount: float
             The amount of water to be poured into the stack
         :return: Glass[]
             An updated list of glasses
         """
-        raise NotImplementedError()
+        self._pour(stack.glasses, water_amount)
+
+        return stack.glasses
+
+    def _pour(self, glasses, water_amount, depth=0):
+        """
+        Recursively pours the water into the stack
+        :param glasses: Glass[]
+            A list of glasses to be poured to
+        :param water_amount: float
+            The amount of inflow water
+        :param depth: int
+            The current depth in the stack
+        """
+        # inflow water to the glass
+        glass = glasses[0]
+        glass.current_capacity += water_amount
+
+        # if the water is outflowing, flow it to the children glasses then update the glass capacity
+        if glass.current_capacity > Glass.MAX_CAPACITY:
+            overflow_amount = glass.current_capacity - Glass.MAX_CAPACITY
+
+            try:
+                # outflow the water to the bottom left and right glasses until no glasses are left (IndexError occurred)
+                self._pour(glasses[depth + 1:], overflow_amount / 2, depth + 1)
+                self._pour(glasses[depth + 2:], overflow_amount / 2, depth + 1)
+            except IndexError:
+                pass
+
+            # update the glass capacity to its maximum
+            glass.current_capacity = Glass.MAX_CAPACITY
 
 
 class StackSearcher:
