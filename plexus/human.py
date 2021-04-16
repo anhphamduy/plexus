7523871213d156a89eb6c5a68055dd0dbb160e4f
Anh Pham <anhphamduy@outlook.com>
2021-04-16 10:40:48 +1000

    Move human and searcher to different files

---
+++ plexus/human.py
@@ -0,0 +1,47 @@
+from plexus.glass import Glass
+
+
+class Human:
+    """ Represents a human doing action entity """
+
+    def pour(self, stack, water_amount):
+        """
+        Pours water into the stack of glasses
+        :param stack: Stack
+            The stack to be poured into
+        :param water_amount: float
+            The amount of water to be poured into the stack
+        :return: Glass[]
+            An updated list of glasses
+        """
+        self._pour(stack.glasses, water_amount)
+
+        return stack.glasses
+
+    def _pour(self, glasses, water_amount, depth=0):
+        """
+        Recursively pours the water into the stack
+        :param glasses: Glass[]
+            A list of glasses to be poured to
+        :param water_amount: float
+            The amount of inflow water
+        :param depth: int
+            The current depth in the stack
+        """
+        # inflow water to the glass
+        glass = glasses[0]
+        glass.current_capacity += water_amount
+
+        # if the water is outflowing, flow it to the children glasses then update the glass capacity
+        if glass.current_capacity > Glass.MAX_CAPACITY:
+            overflow_amount = glass.current_capacity - Glass.MAX_CAPACITY
+
+            try:
+                # outflow the water to the bottom left and right glasses until no glasses are left (IndexError occurred)
+                self._pour(glasses[depth + 1:], overflow_amount / 2, depth + 1)
+                self._pour(glasses[depth + 2:], overflow_amount / 2, depth + 1)
+            except IndexError:
+                pass
+
+            # update the glass capacity to its maximum
+            glass.current_capacity = Glass.MAX_CAPACITY
---
+++ plexus/stack_searcher.py
@@ -0,0 +1,22 @@
+class StackSearcher:
+    """ A searcher to search glasses on stack based on some given criteria """
+
+    def search_glass(self, stack, i, j):
+        """
+        Gets a glass from the stack of the glasses based on the position
+        :param stack: Stack
+            The stack that needs to be searched from
+        :param i: number
+            The x position of the glass
+        :param j: number
+            The y position of the glass
+        :return: Glass
+            The glass that is on (x, y)
+        """
+        if j > i or i < 0 or (i + 1) > stack.n_levels or j < 0:
+            raise ValueError("Invalid inputs")
+
+        # get the level offset in the binary tree array
+        level_offset = ((i + 1) * i) // 2
+
+        return stack.glasses[level_offset + j]
